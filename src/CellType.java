5bc80e4a27b7f1d3d5b9f518841232676c450809
Skolopandre <theo.meurisse@student.unamur.be>
2026-03-11 21:52:57 +0100

    Delete src directory
    delete

--- src/CellType.java
+++
@@ -1,6 +0,0 @@
-public enum CellType {
-    TREE,
-    ASH,
-    EMPTY,
-    WATER
-}
--- src/CloudTypes.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-import java.util.List;
-
-public class CloudTypes {
-    public static final CloudType CUMULONIMBUS = new CloudType("CUMULONIMBUS",5,new Color(50,50,50, 158));
-    public static final CloudType CIRROSTRATUS = new CloudType("CIRROSTRATUS",3,new Color(78, 78, 78, 119));
-    public static final CloudType STRATUS = new CloudType("STRATUS",2,new Color(191, 191, 191, 79));
-    public static final List<CloudType> ALL = List.of(CUMULONIMBUS,CIRROSTRATUS,STRATUS);
-}
